250d9f02fe64f95510b1ca8afe577eb8ce37d7c2
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-04-27 16:55:44 -0300

    Resolução do problema 1008 (salário) - calcula salário de funcionário por horas trabalhadas

---
+++ Beginner/salario_1008.py
@@ -0,0 +1,7 @@
+numero_funcionario = int(input())
+horas_trabalhadas = int(input())
+valor_por_hora = float(input())
+
+salario = horas_trabalhadas * valor_por_hora
+
+print(f"NUMBER = {numero_funcionario}\nSALARY = U$ {salario:.2f}")
