94ecef8523e15949985fd859c1630d0519472962
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-04-29 14:55:21 -0300

    Resolução do problema 1042 (classificação simples) - ordena três valores mantendo ordem original na saída

---
+++ Beginner/classificacao_simples_1042.py
@@ -0,0 +1,17 @@
+a, b, c = list(map(int, input().split()))
+
+original = [a, b, c]
+ordenada = sorted(original)
+
+for n in ordenada:
+    print(n)
+
+print()
+
+for n in original:
+    print(n)
+
+
+
+
+
